877202e20cc7fc66dcee316ee0943c733f49b2c1
José de los santos <delossantosramosjose@gmail.com>
2025-10-05 20:49:35 +0200

    in package.json added build:ghpages and addapt flow when deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+// Detectar si es build para GitHub Pages
+const isGitHubPages = process.env.BUILD_FOR_GHPAGES === 'true';
+
 export default defineConfig({
-  site: 'https://paginasexternas.github.io',
-  base: '/cleanandcare.es',
+  site: isGitHubPages ? 'https://paginasexternas.github.io' : 'https://cleanandcare.es',
+  base: isGitHubPages ? '/cleanandcare.es' : '/',
   integrations: [tailwind()],
   experimental: {
     viewTransitions: true
   },
   build: {
     assets: '_astro'
-  }
+  },
+  // Configuración adicional para mejorar la compatibilidad con GitHub Pages
+  output: 'static',
+  trailingSlash: 'ignore'
 });
